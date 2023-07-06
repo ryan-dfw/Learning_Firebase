870aa4613a1a789c78774bad7722014dd54366fc
gdknas <mckevitt.r@gmail.com>
2023-07-05 21:44:40 -0500

    added read functionality to userName

--- app/src/main/java/com/example/learning_firebase/MainActivity.kt
+++ app/src/main/java/com/example/learning_firebase/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var editTextName : EditText
     lateinit var buttonSend : Button
+    lateinit var textViewName: TextView
 
     val database : FirebaseDatabase = FirebaseDatabase.getInstance()
     val reference : DatabaseReference = database.reference.child("Users")
+    val reference2 : DatabaseReference = database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,19 @@ class MainActivity : AppCompatActivity() {
 
         editTextName = findViewById(R.id.editTextName)
         buttonSend = findViewById(R.id.buttonSend)
+        textViewName = findViewById(R.id.textViewName)
+
+        reference2.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val realName : String = snapshot.child("Users").child("name").value as String
+                textViewName.text = realName
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
 
         buttonSend.setOnClickListener {
             val userName : String = editTextName.text.toString()
